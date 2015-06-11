38f7d7e7fb884c7d78333c0cf1247a0d2f54b8fc
kohterai <koh.terai@nyu.edu>
2015-06-11 22:06:44 +0400

    Fixture format updated

--- utilities/fixtures.js
+++ utilities/fixtures.js
@@ -1,22 +1,31 @@
-fixtureItems = [{
-      restaurantName: "Kohs Awesome Restaurant",
-      latitude: "10.203",
-      longtitude: "20.3422",
-      menu: {
-      	rating: 0,
-      	price: 100,
-      	catagory: ["vegan", "puppies"]
-      }
-    },
-    {
-      restaurantName: "Kohs Awesome Restaurant 2",
-      latitude: "10.203",
-      longtitude: "20.3422",
-      menu: {
-      	rating: 0,
-      	price: 100,
-      	catagory: ["vegan", "puppies"]
-      }
+restaurants = [{
+		_id: 1,
+		name: "Kohs Awesome restuarant",
+		latitude: 10.239823,
+		longtitude: 20.33322
+	},
+	{
+		_id: 2,
+		name: "Quans Aweoms.",
+		latitude: 121.1212,
+		longtitude: 211121
+	}
+]
+
+
+menuItems = [{
+      restaurantID: 1,
+      name: "Chicken Curry",
+			rating: 0,
+			price: 100,
+			catagory: "main dish",
+			filter: ["vegan", "puppies"],
+			reviews: [{
+				user: "Koh",
+				date: new Date(),
+				comment: "This place is the best"
+			}],
+			date: new Date()
     }
     ]
 
@@ -24,8 +33,8 @@ Meteor.methods({
 	'fixtures': function() {
 		Data.remove({})
 		console.log("database dropped")
-		for (i = 0; i < fixtureItems.length; i++) {
-			Data.insert(fixtureItems[i]);
+		for (i = 0; i < menuItems.length; i++) {
+			Data.insert(menuItems[i]);
 		}
     console.log("fixtures inserted")
 	}
