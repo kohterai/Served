86024b6895d33c109ed5cbe8b9084c27b2185dde
kohterai <koh.terai@nyu.edu>
2015-06-12 10:18:51 +0400

    Trying to get routing to work.

--- utilities/router.js
+++ utilities/router.js
@@ -6,10 +6,22 @@ Router.route('/login', function () {
   this.render('Login');
 });
 
-Router.route('/feed', function () {
-  this.render('feed');
-});
-
 Router.route('/addItem', function () {
   this.render('addItem');
 });
+
+
+Router.route('/feed/:_id', function () {
+  this.render('feed', {
+    data: function () {
+    	// console.log(Restaurants.findOne({name: this.params._id}))
+      return Restaurants.findOne({name: this.params._id});
+    }
+  });
+});
+
+// Router.route('/feed/:_id/:foodName', function () {
+//   this.render('details', {
+//   	name: 'item.name'
+//   });
+// });
